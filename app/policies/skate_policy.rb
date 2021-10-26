308eeb10636a5c6ad92926a2892449ddc8dbd6e2
daniel vasquez <danieleduardovasquezgarcia@gmail.com>
2021-10-26 15:32:05 -0400

    edit

--- app/policies/skate_policy.rb
+++ app/policies/skate_policy.rb
@@ -9,6 +9,15 @@ class SkatePolicy < ApplicationPolicy
     return true
   end
 
+  def edit?
+    # user es igual a current user y ve si el usuario de la secion es el mismo para poder editar
+    user == record.user
+  end
+
+  def update?
+    user == record.user
+  end
+
   def new?
     return true
   end
